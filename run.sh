085889ec39402c56c28183b8094a69654730eacc
Bruno Munoz <bruno.munoz@orange.com>
2018-05-14 14:23:38 +0200

    add env.sh template

--- run.sh
+++ run.sh
@@ -2,16 +2,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+source $DIR/env.sh
+
 NAME=cntlm
+
+docker network create proxy 2>/dev/null
 
 docker container stop $NAME >/dev/null
 docker container rm $NAME >/dev/null
 docker container run --name $NAME \
   --restart always \
-  -p 127.0.0.1:3128:3128 \
+  --network=proxy \
+  -p $CNTLM_LISTEN_IP:3128:3128 \
   -e USERNAME=$CNTLM_USERNAME \
   -e DOMAIN=$CNTLM_DOMAIN \
   -e PASSNTLMV2=$CNTLM_PASSNTLMV2 \
   -e PROXY=$CNTLM_PROXY \
+  -e OPTIONS="-w $CNTLM_WORKSTATION" \
   -d \
   $NAME:latest
